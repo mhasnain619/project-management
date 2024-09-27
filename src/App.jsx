db51ac003d3d15980b694c45c777e307477a9d5d
Muhammad Hasnain <hasnain0893@gmail.com>
2024-09-27 12:05:20 +0500

    create project button working

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 // import NewProject from "./components/NewProject";
+import { useState } from "react";
 import NoProjectSelected from "./components/NoProjectSelected";
 import Sidebar from "./components/Sidebar";
+import NewProject from "./components/NewProject";
 
 function App() {
+  const [prjectsState, setPrjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  function handleStartAddProject() {
+    setPrjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      }
+    })
+  }
+  let content;
+  if (prjectsState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (prjectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NoProjectSelected />
-      {/* <NewProject /> */}
+      <Sidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
