ce41352e4addf0c63ebe9e2614094170c7038e61
Muhammad Hasnain <hasnain0893@gmail.com>
2024-09-27 11:48:50 +0500

    working

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <Sidebar />
+      <NoProjectSelected />
+      {/* <NewProject /> */}
+    </main>
+  );
+}
+
+export default App;
