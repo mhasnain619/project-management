d92275ed8329c3f218daf9089929f37ff53dff63
Muhammad Hasnain <hasnain0893@gmail.com>
2024-09-28 11:06:29 +0500

    add select delete

--- src/App.jsx
+++ src/App.jsx
@@ -52,9 +52,19 @@ function App() {
       }
     })
   }
+  function handleDeleteProject() {
+    setProjectsState(prevState => {
+
+      return {
+        ...prevState,
+        selectedProjectId: null,
+        projects: prevState.projects.filter((project) => project.id !== prevState.selectedProjectId)
+      }
+    })
+  }
   const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId)
 
-  let content = <SelectedProjects project={selectedProject} />
+  let content = <SelectedProjects project={selectedProject} onDelete={handleDeleteProject} />
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancel} />
   } else if (projectsState.selectedProjectId === undefined) {
--- src/components/SelectedProjects.jsx
+++ src/components/SelectedProjects.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import Button from './Button'
 
-const SelectedProjects = ({ project }) => {
+const SelectedProjects = ({ project, onDelete }) => {
     const formatedDate = new Date(project.dueDate).toLocaleDateString('en-US', {
         year: 'numeric',
         month: 'short',
@@ -13,7 +12,7 @@ const SelectedProjects = ({ project }) => {
             <header className='pb-4 mb-4 border-b-2 border-stone-300'>
                 <div className='flex items-center justify-between'>
                     <h1 className='text-3xl font-bold text-stone-600 mb-2'>{project.title}</h1>
-                    <Button>delete</Button>
+                    <button className='text-stone-800 hover:text-stone-950' onClick={onDelete}>delete</button>
                 </div>
                 <p className='mb-4 text-stone-400'>{formatedDate}</p>
                 <p className='text-stone-600 whitespace-pre-wrap'>{project.description}</p>
