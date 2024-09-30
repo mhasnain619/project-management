0f9c377fcff484cc0711460147a50f1ae36ff7b2
Muhammad Hasnain <hasnain0893@gmail.com>
2024-09-30 19:49:16 +0500

    new task

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -6,13 +6,12 @@ const NewTask = ({ onAdd }) => {
         setEnteredTask(e.target.value)
     }
     function handleClick() {
-        debugger
         onAdd(enteredTask)
         setEnteredTask('')
     }
     return (
         <div className='flex items-center gap-4'>
-            <input onChange={handleChange} value={enteredTask} type="text" className='w-60 px-2 py-1 rounded-sm bg-stone-200' />
+            <input onChange={handleChange} value={enteredTask} type="text" className='w-64 px-2 py-1 rounded-sm bg-stone-200' />
             <button onClick={handleClick} className='text-stone-700 hover:text-stone-900'>Add Task</button>
         </div>
     )
